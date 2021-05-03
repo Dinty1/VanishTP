68713559af1f6cbd361c65150c5ff1b0153516a9
Dinty1 <dintybmrt@gmail.com>
2021-05-03 10:57:24 +0100

    Version to 2.0.0-SNAPSHOT

---
+++ src/main/java/io/github/dinty1/vanishtp/listeners/SuperVanishListener.java
@@ -0,0 +1,27 @@
+/*
+Listens to the SuperVanish vanish status change event
+ */
+package io.github.dinty1.vanishtp.listeners;
+
+import de.myzelyam.api.vanish.PlayerVanishStateChangeEvent;
+import io.github.dinty1.vanishtp.VanishManager;
+import io.github.dinty1.vanishtp.VanishTP;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public class PlayerVanishStatusChangeListener implements Listener {
+    private VanishTP plugin;
+
+    public PlayerVanishStatusChangeListener(VanishTP p) {
+        this.plugin = p;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    @SuppressWarnings("unused")
+    public void onVanishStatusChange(PlayerVanishStateChangeEvent event) {
+        final Player player = plugin.getServer().getPlayer(event.getName());
+        VanishManager.onVanishStatusChange(player, event.isVanishing(), plugin);
+    }
+}
